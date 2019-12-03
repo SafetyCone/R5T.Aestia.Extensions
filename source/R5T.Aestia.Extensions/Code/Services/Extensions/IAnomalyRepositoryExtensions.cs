57cf4255236c2ba62215beb8c55db7c0c612c820
MinexAutomation <minex.automation@gmail.com>
2019-12-02 22:00:27 -0800

    Added an extension to set the anomaly reported UTC date-time using an R5T.Votadina ICurrentUtcDateTimeProvider service instance.

---
+++ source/R5T.Aestia.Extensions/Code/Services/Extensions/IAnomalyRepositoryExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using R5T.Sindia;
+using R5T.Votadinia;
+
+
+namespace R5T.Aestia
+{
+    public static class IAnomalyRepositoryExtensions
+    {
+        /// <summary>
+        /// Create a new anomaly (identity) and set the reported UTC time to the current UTC time.
+        /// </summary>
+        public static AnomalyIdentity NewSetReportedUTC(this IAnomalyRepository repository, ICurrentUtcDateTimeProvider currentUtcDateTimeProvider)
+        {
+            var anomalyIdentity = repository.New();
+
+            var nowUTC = currentUtcDateTimeProvider.GetCurrentUtcDateTime();
+
+            repository.SetReportedUTC(anomalyIdentity, nowUTC);
+
+            return anomalyIdentity;
+        }
+    }
+}
